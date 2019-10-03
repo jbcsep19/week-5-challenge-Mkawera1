0387d75f27af5bf084e0d28d9b092b83096a8e48
Mkawera1 <GBTC441003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-03 18:05:30 -0400

    Guest Book

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    PostRepository postRepository;
+    @RequestMapping("/")
+    public String listPosts(Model model){
+        model.addAttribute("posts", postRepository.findAll());
+        return "list";
+    }
+    @GetMapping("/add")
+    public String postForm(Model model){
+        model.addAttribute("post", new Post());
+        return "postform";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Post post,
+                              BindingResult result){
+        if(result.hasErrors()){
+            return "postform";
+        }
+        postRepository.save(post);
+        return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String viewPost(@PathVariable("id") long id, Model model)
+    {model.addAttribute("post", postRepository.findById(id).get());
+    return "view";
+    }
+    @RequestMapping("/update/{id}")
+    public String updatePost(@PathVariable("id") long id,
+                             Model model){
+        model.addAttribute("post", postRepository.findById(id).get());
+        return "postform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delPost(@PathVariable("id") long id){
+        postRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
